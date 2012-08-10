0c600676b366d06b0d0ceefcb1aa3a09958355f8
Bruno Chagas <brunochaagsweb@gmail.com>
2012-08-10 18:21:04 -0300

    First commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'locomotive_cms', '~> 2.0.0.rc9', :require => 'locomotive/engine'
+gem 'rails', '3.2.8'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'compass-rails'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'unicorn'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bchagas::Application.config.secret_token = '047e9b319404851eb049fd309a9f6355e1d3a49be33d46bee18d8244546d523b3b78c9bb6f097ec472ea4da56ba4da1adf6b5a2c26a242e1660b351b0f8fbb04'
